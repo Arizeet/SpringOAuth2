d8c7cf5d8277b54387fdd91284edfbed6a3b43b9
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2024-10-18 22:17:03 +0530

    added OAuth2 clients (Google & Github)

---
+++ src/main/java/com/arizeet/SpringOAuth2/HomeController.java
@@ -0,0 +1,13 @@
+package com.arizeet.SpringOAuth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Hello Spring OAuth2";
+    }
+}
